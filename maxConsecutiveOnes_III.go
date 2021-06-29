53d5344c2e172ac32d20056aa07821f6d0b86129
kur0yan <rithvikmarri@live.com>
2021-06-29 14:50:24 -0500

    Added initial three files

---
+++ maxConsecutiveOnes_III.go
@@ -0,0 +1,55 @@
+/*
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+	Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+	Output: 6
+	Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+
+Example 2:
+	Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+	Output: 10
+	Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+
+Constraints:
+    1 <= nums.length <= 105
+    nums[i] is either 0 or 1.
+    0 <= k <= nums.length
+*/
+
+package leetcode
+
+func longestOnes(nums []int, k int) int {
+	windowStart, windowEnd, nz, maxLength, winLength := 0, 0, 0, 0, 1
+
+	if nums[windowEnd] == 0 {
+		nz++
+	}
+	for windowEnd < len(nums) {
+		windowEnd++
+
+		if windowEnd >= len(nums) {
+			return maxLength
+		}
+
+		if nums[windowEnd] == 0 {
+			nz++
+		}
+
+		winLength++
+
+		for nz > k {
+			windowStart++
+			if nums[windowStart-1] == 0 {
+				nz--
+			}
+			winLength--
+		}
+
+		if winLength > maxLength {
+			maxLength = winLength
+		}
+	}
+
+	return maxLength
+}
---
+++ rmDupSortedArray.go
@@ -0,0 +1,37 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.The relative order of the elements should be kept the same. Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+package goleetcode
+
+func removeDuplicates(nums []int) int {
+	curr, numPos, k := 0, 0, 1
+
+	if len(nums) == 0 {
+		return 0
+	}
+
+	for numPos < len(nums) {
+		numPos++
+
+		if numPos >= len(nums) {
+			return k
+		}
+
+		if nums[numPos] == nums[curr] {
+			nums[numPos] = 999
+		} else if nums[numPos] == 999 {
+			//Do Nothing
+		} else if curr+1 >= len(nums) {
+			return k
+		} else {
+			nums[numPos], nums[curr+1] = nums[curr+1], nums[numPos]
+			curr++
+			k++
+		}
+	}
+	return k
+}
